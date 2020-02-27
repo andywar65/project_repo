49dcb11934f4c534fed7e8dc1ce9419001d3c700
andywar65 <andy.war1965@gmail.com>
2020-02-27 19:47:23 +0100

    cleaned pagine views

--- pagine/views.py
+++ pagine/views.py
@@ -1,33 +1,19 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import (ListView, DetailView, CreateView,
     TemplateView)
-from django.views.generic.dates import (ArchiveIndexView, YearArchiveView,
-    MonthArchiveView, DayArchiveView, )
 from taggit.models import Tag
 
 from .forms import UserUploadForm
-from .models import (Location, Event, UserUpload, Blog)
+from .models import (UserUpload, Blog)
 
 class HomeTemplateView(TemplateView):
     template_name = 'home.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['all_events'] = Event.objects.all()[:6]
         context['posts'] = Blog.objects.all()[:6]
         return context
-
-class ListLocation(ListView):
-    model = Location
-    context_object_name = 'all_locations'
-    paginate_by = 12
-
-class DetailLocation(DetailView):
-    model = Location
-    context_object_name = 'location'
-    slug_field = 'slug'
 
 class TagMixin:
     def get_context_data(self, **kwargs):
@@ -42,48 +28,6 @@ class TagMixin:
         if 'categoria' in self.request.GET:
             qs = qs.filter(tags__name=self.request.GET['categoria'])
         return qs
-
-class EventArchiveIndexView(TagMixin, ArchiveIndexView):
-    model = Event
-    date_field = 'date'
-    allow_future = True
-    context_object_name = 'all_events'
-    paginate_by = 12
-    allow_empty = True
-
-class EventYearArchiveView(TagMixin, YearArchiveView):
-    model = Event
-    make_object_list = True
-    date_field = 'date'
-    allow_future = True
-    context_object_name = 'all_events'
-    paginate_by = 12
-    year_format = '%Y'
-    allow_empty = True
-
-class EventMonthArchiveView(TagMixin, MonthArchiveView):
-    model = Event
-    date_field = 'date'
-    allow_future = True
-    context_object_name = 'all_events'
-    year_format = '%Y'
-    month_format = '%m'
-    allow_empty = True
-
-class EventDayArchiveView(TagMixin, DayArchiveView):
-    model = Event
-    date_field = 'date'
-    allow_future = True
-    context_object_name = 'all_events'
-    year_format = '%Y'
-    month_format = '%m'
-    day_format = '%d'
-    allow_empty = True
-
-class DetailEvent(DetailView):
-    model = Event
-    context_object_name = 'event'
-    slug_field = 'slug'
 
 class ListBlog(TagMixin, ListView):
     model = Blog
@@ -110,8 +54,6 @@ class UserUploadCreateView(LoginRequiredMixin, CreateView):
 
     def form_valid(self, form):
         form.instance.user = self.request.user
-        if 'event_id' in self.request.GET:
-            form.instance.event = Event.objects.get(id=self.request.GET['event_id'])
-        elif 'post_id' in self.request.GET:
+        if 'post_id' in self.request.GET:
             form.instance.post = Blog.objects.get(id=self.request.GET['post_id'])
         return super().form_valid(form)
