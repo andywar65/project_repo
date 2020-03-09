af581d9a6dac625995316d10a6a394c86e111be1
andywar65 <andy.war1965@gmail.com>
2020-03-10 00:19:15 +0100

    updated project search view

--- project/views/search.py
+++ project/views/search.py
@@ -1,7 +1,8 @@
 from django import forms
+from django.contrib.contenttypes.models import ContentType
 from django.shortcuts import render
 from django.db.models import Q
-from pagine.models import (Event, Blog, EventUpgrade, UserUpload)
+from pagine.models import (Blog, UserUpload)
 
 class ValidateForm(forms.Form):
     q = forms.CharField(max_length=100)
@@ -11,22 +12,22 @@ def search_results(request):
     form = ValidateForm(request.GET)
     if form.is_valid():
         q = request.GET['q']
-        uploads = UserUpload.objects.filter(body__icontains = q)
-        up_list = uploads.values_list('event_id', flat = True)
-        upgrades = EventUpgrade.objects.filter(Q(title__icontains=q)|
-            Q(body__icontains=q)).values_list('event_id', flat = True)
-        events = Event.objects.filter(Q(title__icontains=q)|
-            Q(intro__icontains=q)|Q(body__icontains=q)|
-            Q(chronicle__icontains=q)|Q(restricted__icontains=q)|
-            Q(id__in = upgrades)|Q(id__in = up_list))
-        if events:
-            success = True
+        #prepare list of indexed paragraphs containing query
+        paragraphs = IndexedParagraph.objects.filter(Q(body__icontains = q)|
+            Q(title__icontains = q))
+        #blog content type
+        bl_type = ContentType.objects.get(app_label='pagine', model='blog').id
+        #filter paragraphs by blog type
+        bl_paragraphs = paragraphs.filter( parent_type = bl_type )
+        #extract list of blogs
+        bl_list = bl_paragraphs.values_list('parent_id', flat = True)
+        #prepare list of user uploads referenced by blog posts
         up_list = uploads.values_list('post_id', flat = True)
         blogs = Blog.objects.filter(Q(title__icontains=q)|
-            Q(intro__icontains=q)|Q(body__icontains=q)|Q(id__in = up_list))
+            Q(intro__icontains=q)|Q(id__in = up_list)|Q(id__in = bl_list))
         if blogs:
             success = True
         return render(request, 'search_results.html', {'search': q,
-            'all_events': events, 'all_blogs': blogs, 'success': success})
+            'all_blogs': blogs, 'success': success})
     else:
         return render(request, 'search_results.html', {'success': success, })
