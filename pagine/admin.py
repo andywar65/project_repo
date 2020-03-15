edc3079fdc451826c8e0cd68b8c26ded30e0d359
andywar65 <andy.war1965@gmail.com>
2020-03-15 23:51:02 +0100

    deleted send notice action

--- pagine/admin.py
+++ pagine/admin.py
@@ -1,9 +1,6 @@
-from django.conf import settings
-from django.core.mail import EmailMessage
 from django.contrib import admin
 from .models import ( UserUpload, Blog, HomePage, Institutional)
 from .forms import BlogForm
-from users.models import Profile
 
 class UserUploadInline(admin.TabularInline):
     model = UserUpload
@@ -16,26 +13,6 @@ class BlogAdmin(admin.ModelAdmin):
     search_fields = ('title', 'date', 'intro', )
     inlines = [ UserUploadInline,  ]
     form = BlogForm
-    #actions = ['send_notice', ]
-
-    def send_notice(self, request, queryset):
-        for post in queryset:
-            message = post.title + '\n'
-            message += post.intro + '\n'
-            url = settings.BASE_URL + post.get_path()
-            message += 'Fai click su questo link per leggerlo: ' + url + '\n'
-            recipients = Profile.objects.filter(
-                user__is_active = True, no_spam = True, )
-            mailto = []
-            for recipient in recipients:
-                mailto.append(recipient.user.email)
-            subject = 'Nuovo articolo'
-            email = EmailMessage(subject, message, settings.SERVER_EMAIL,
-                [mailto])
-            email.send()
-            post.notice = 'DONE'
-            post.save()
-    send_notice.short_description = 'Invia notifiche'
 
 @admin.register(Institutional)
 class InstitutionalAdmin(admin.ModelAdmin):
