e899ccd1fd73fe35c163cbc4f3c0b7963a475b5d
andywar65 <andy.war1965@gmail.com>
2020-03-19 22:23:19 +0100

    deleting unused pagine views

--- pagine/urls/locations.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from pagine.views import ListLocation, DetailLocation
-
-urlpatterns = [
-    path('', ListLocation.as_view(), name='locations'),
-    path('<slug>/', DetailLocation.as_view(), name='location'),
-    ]
