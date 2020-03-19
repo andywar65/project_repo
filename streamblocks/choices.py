994b9aeabe884cb0e8180bf3483cc6ae6af82c20
andywar65 <andy.war1965@gmail.com>
2020-03-19 23:45:16 +0100

    added choices to boxed text streamblock model

--- streamblocks/choices.py
+++ streamblocks/choices.py
@@ -1,4 +1,5 @@
 HEIGHT = [('1', 'Troppo grande'), ('2', 'Molto grande'), ('3', 'Grande'),
     ('4', 'Medio'), ('5', 'Piccolo'), ('6', 'Molto piccolo'),]
 
-COLOR = [('success', 'Verde'), ('warning', 'Giallo'), ('danger', 'Rosso'), ] 
+COLOR = [('success', 'Verde'), ('warning', 'Giallo'), ('danger', 'Rosso'),
+    ('primary', 'Blu'), ('secondary', 'Grigio'), ] 
