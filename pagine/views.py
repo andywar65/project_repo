b79650020927e5135a249512070b2b08dce99fd5
andywar65 <andy.war1965@gmail.com>
2020-03-19 22:47:59 +0100

    adding blog date views

--- pagine/views.py
+++ pagine/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import (ListView, DetailView, CreateView,
     TemplateView)
+from django.views.generic.dates import (ArchiveIndexView, YearArchiveView,
+    MonthArchiveView, DayArchiveView, )
 from taggit.models import Tag
 from streamblocks.models import HomeButton
 
@@ -37,11 +39,42 @@ class TagMixin:
             qs = qs.filter(tags__name=self.request.GET['categoria'])
         return qs
 
-class ListBlog(TagMixin, ListView):
+class BlogArchiveIndexView(TagMixin, ArchiveIndexView):
     model = Blog
-    ordering = ('-date', )
+    date_field = 'date'
+    allow_future = True
     context_object_name = 'posts'
     paginate_by = 12
+    allow_empty = True
+
+class BlogYearArchiveView(TagMixin, YearArchiveView):
+    model = Blog
+    make_object_list = True
+    date_field = 'date'
+    allow_future = True
+    context_object_name = 'posts'
+    paginate_by = 12
+    year_format = '%Y'
+    allow_empty = True
+
+class BlogMonthArchiveView(TagMixin, MonthArchiveView):
+    model = Blog
+    date_field = 'date'
+    allow_future = True
+    context_object_name = 'posts'
+    year_format = '%Y'
+    month_format = '%m'
+    allow_empty = True
+
+class BlogDayArchiveView(TagMixin, DayArchiveView):
+    model = Blog
+    date_field = 'date'
+    allow_future = True
+    context_object_name = 'posts'
+    year_format = '%Y'
+    month_format = '%m'
+    day_format = '%d'
+    allow_empty = True
 
 class DetailBlog(DetailView):
     model = Blog
@@ -53,12 +86,10 @@ class UserUploadCreateView(LoginRequiredMixin, CreateView):
     form_class = UserUploadForm
 
     def get_success_url(self):
-        if 'event_id' in self.request.GET:
-            evt = Event.objects.get(id=self.request.GET['event_id'])
-            return evt.get_path() + '/#upload-anchor'
-        elif 'post_id' in self.request.GET:
+        if 'post_id' in self.request.GET:
             pst = Blog.objects.get(id=self.request.GET['post_id'])
             return pst.get_path() + '/#upload-anchor'
+        return super(UserUploadCreateView, self).get_success_url(self)
 
     def form_valid(self, form):
         form.instance.user = self.request.user
