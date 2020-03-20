b98a0679f6c83a8dbca3721f7d91a266c9d561c8
andywar65 <andy.war1965@gmail.com>
2020-03-20 18:28:13 +0100

    deleting blog and user upload model from pagine app

--- pagine/admin.py
+++ pagine/admin.py
@@ -1,18 +1,5 @@
 from django.contrib import admin
-from .models import ( UserUpload, Blog, HomePage, Institutional)
-from .forms import BlogForm
-
-class UserUploadInline(admin.TabularInline):
-    model = UserUpload
-    fields = ('user', 'date', 'image', 'body', )
-    extra = 0
-
-@admin.register(Blog)
-class BlogAdmin(admin.ModelAdmin):
-    list_display = ('title', 'date', 'author', 'notice', )
-    search_fields = ('title', 'date', 'intro', )
-    inlines = [ UserUploadInline,  ]
-    form = BlogForm
+from .models import ( HomePage, Institutional)
 
 @admin.register(Institutional)
 class InstitutionalAdmin(admin.ModelAdmin):
--- pagine/views.py
+++ pagine/views.py
@@ -3,13 +3,10 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import (ListView, DetailView, CreateView,
     TemplateView)
-from django.views.generic.dates import (ArchiveIndexView, YearArchiveView,
-    MonthArchiveView, DayArchiveView, )
 from taggit.models import Tag
 from streamblocks.models import HomeButton
 
-from .forms import UserUploadForm
-from .models import (UserUpload, Blog, HomePage, Institutional)
+from .models import ( HomePage, Institutional)
 
 class HomeTemplateView(TemplateView):
     template_name = 'pagine/home.html'
@@ -24,78 +21,6 @@ class HomeTemplateView(TemplateView):
         for action in actions:
             context['actions'] = HomeButton.objects.filter(id__in = action['id'])[:3]
         return context
-
-class TagMixin:
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['tags'] = Tag.objects.all()
-        if 'categoria' in self.request.GET:
-            context['tag_filter'] = self.request.GET['categoria']
-        return context
-
-    def get_queryset(self):
-        qs = super().get_queryset()
-        if 'categoria' in self.request.GET:
-            qs = qs.filter(tags__name=self.request.GET['categoria'])
-        return qs
-
-class BlogArchiveIndexView(TagMixin, ArchiveIndexView):
-    model = Blog
-    date_field = 'date'
-    allow_future = True
-    context_object_name = 'posts'
-    paginate_by = 12
-    allow_empty = True
-
-class BlogYearArchiveView(TagMixin, YearArchiveView):
-    model = Blog
-    make_object_list = True
-    date_field = 'date'
-    allow_future = True
-    context_object_name = 'posts'
-    paginate_by = 12
-    year_format = '%Y'
-    allow_empty = True
-
-class BlogMonthArchiveView(TagMixin, MonthArchiveView):
-    model = Blog
-    date_field = 'date'
-    allow_future = True
-    context_object_name = 'posts'
-    year_format = '%Y'
-    month_format = '%m'
-    allow_empty = True
-
-class BlogDayArchiveView(TagMixin, DayArchiveView):
-    model = Blog
-    date_field = 'date'
-    allow_future = True
-    context_object_name = 'posts'
-    year_format = '%Y'
-    month_format = '%m'
-    day_format = '%d'
-    allow_empty = True
-
-class DetailBlog(DetailView):
-    model = Blog
-    context_object_name = 'post'
-    slug_field = 'slug'
-
-class UserUploadCreateView(LoginRequiredMixin, CreateView):
-    model = UserUpload
-    form_class = UserUploadForm
-
-    def get_success_url(self):
-        if 'post_id' in self.request.GET:
-            pst = Blog.objects.get(id=self.request.GET['post_id'])
-            return pst.get_path() + '/#upload-anchor'
-        return super(UserUploadCreateView, self).get_success_url(self)
-
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-        if 'post_id' in self.request.GET:
-            form.instance.post = Blog.objects.get(id=self.request.GET['post_id'])
-        return super().form_valid(form)
 
 #this is used by different institutional pages depending on slug
 def get_page_by_slug(context, slug):
