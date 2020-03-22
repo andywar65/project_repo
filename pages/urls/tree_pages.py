b3d72de7a5edf0457b4d50f1890718c7a41557ef
andywar65 <andy.war1965@gmail.com>
2020-03-22 17:49:05 +0100

    deleting migrations

--- pages/urls/tree_pages.py
+++ pages/urls/tree_pages.py
@@ -2,8 +2,6 @@ from django.urls import path, re_path
 from pages.views import (TreePageDetailView, TreePageListView,
     page_by_path)
 
-#pretty lousy way to get things done :)
-
 app_name = 'docs'
 urlpatterns = [
     path('', TreePageListView.as_view(), name = 'page_list'),
