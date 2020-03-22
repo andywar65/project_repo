2ba2c52400f96f96a891c7407ef32596700ce704
andywar65 <andy.war1965@gmail.com>
2020-03-22 01:38:32 +0100

    adding page list to tree pages urlpatterns

--- pages/urls/tree_pages.py
+++ pages/urls/tree_pages.py
@@ -1,18 +1,19 @@
 from django.urls import path
-from pages.views import TreePageDetailView
+from pages.views import TreePageDetailView, TreePageListView
 
 #pretty lousy way to get things done :)
 
 app_name = 'docs'
 urlpatterns = [
-    path('<slug>/', TreePageDetailView.as_view(), name = 'page_detail'),
-    path('<a>/<slug>/', TreePageDetailView.as_view(), name = 'page_detail'),
+    path('', TreePageListView.as_view(), name = 'page_list'),
+    path('<slug>/', TreePageDetailView.as_view(), name = 'page_detail_1'),
+    path('<a>/<slug>/', TreePageDetailView.as_view(), name = 'page_detail_2'),
     path('<a>/<b>/<slug>/',
-        TreePageDetailView.as_view(), name = 'page_detail'),
+        TreePageDetailView.as_view(), name = 'page_detail_3'),
     path('<a>/<b>/<c>/<slug>/',
-        TreePageDetailView.as_view(), name = 'page_detail'),
+        TreePageDetailView.as_view(), name = 'page_detail_4'),
     path('<a>/<b>/<c>/<d>/<slug>/',
-        TreePageDetailView.as_view(), name = 'page_detail'),
+        TreePageDetailView.as_view(), name = 'page_detail_5'),
     path('<a>/<b>/<c>/<d>/<e>/<slug>/',
-        TreePageDetailView.as_view(), name = 'page_detail'),
+        TreePageDetailView.as_view(), name = 'page_detail_6'),
     ]
