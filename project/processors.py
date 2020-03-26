105aa797d817542ca2eebbe9e69558945dbeb97f
andywar65 <andy.war1965@gmail.com>
2020-03-26 10:41:25 +0100

    adding a custom context processor to project

---
+++ project/processors.py
@@ -0,0 +1,4 @@
+from django.conf import settings
+
+def get_global_settings(request):
+    return
--- project/settings/base.py
+++ project/settings/base.py
@@ -66,6 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'project.processors.get_global_settings',
             ],
         },
     },
