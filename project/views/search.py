bfd2b95625f6e613ec064b9f316893dd5bb3c476
andywar65 <andy.war1965@gmail.com>
2020-03-29 23:40:19 +0200

    search view looks in stream rendered

--- project/views/search.py
+++ project/views/search.py
@@ -1,11 +1,8 @@
 from django import forms
-from django.contrib.contenttypes.models import ContentType
 from django.shortcuts import render
 from django.db.models import Q
 from blog.models import Article, UserUpload
 from pages.models import TreePage
-from streamblocks.models import IndexedParagraph
-from helper.models import StreamHelper
 
 class ValidateForm(forms.Form):
     q = forms.CharField(max_length=100)
@@ -15,33 +12,17 @@ def search_results(request):
     form = ValidateForm(request.GET)
     if form.is_valid():
         q = request.GET['q']
-        #prepare list of indexed paragraphs containing query
-        paragraphs = IndexedParagraph.objects.filter(Q(body__icontains = q)|
-            Q(title__icontains = q)).values_list('id', flat = True)
-        #prepare queryset of helpers of the indexed paragraph type in the list
-        helpers = StreamHelper.objects.filter(stream_type='IndexedParagraph',
-            stream_id__in=paragraphs, )
-        #blog content type
-        bl_type = ContentType.objects.get(app_label='blog', model='article').id
-        #filter paragraphs by blog type
-        bl_helpers = helpers.filter( obj_type = bl_type )
-        #extract list of blogs
-        bl_list = bl_helpers.values_list('obj_id', flat = True)
         #prepare list of user uploads referenced by blog posts
         uploads = UserUpload.objects.filter(body__icontains = q)
-        up_list = uploads.values_list('post_id', flat = True)
+        uploads = uploads.values_list('post_id', flat = True)
         blogs = Article.objects.filter(Q(title__icontains=q)|
-            Q(intro__icontains=q)|Q(id__in = up_list)|Q(id__in = bl_list))
+            Q(intro__icontains=q)|Q(stream_rendered__icontains = q)|
+            Q(id__in = uploads))
         if blogs:
             success = True
         #TreePage page content type
-        tp_type = ContentType.objects.get(app_label='pages', model='treepage').id
-        #filter paragraphs by blog type
-        tp_helpers = helpers.filter( obj_type = tp_type )
-        #extract list of TreePage
-        tp_list = tp_helpers.values_list('obj_id', flat = True)
         pages = TreePage.objects.filter(Q(title__icontains=q)|
-            Q(intro__icontains=q)|Q(id__in = tp_list))
+            Q(intro__icontains=q)|Q(stream_rendered__icontains = q))
         if pages:
             success = True
         return render(request, 'search_results.html', {'search': q,
