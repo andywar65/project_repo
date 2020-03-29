2de387ba3a42515044e374fe82c0d29924ad203e
andywar65 <andy.war1965@gmail.com>
2020-03-29 22:28:34 +0200

    search view uses stream helpers

--- project/views/search.py
+++ project/views/search.py
@@ -5,6 +5,7 @@ from django.db.models import Q
 from blog.models import Article, UserUpload
 from pages.models import TreePage
 from streamblocks.models import IndexedParagraph
+from helper.models import StreamHelper
 
 class ValidateForm(forms.Form):
     q = forms.CharField(max_length=100)
@@ -16,13 +17,16 @@ def search_results(request):
         q = request.GET['q']
         #prepare list of indexed paragraphs containing query
         paragraphs = IndexedParagraph.objects.filter(Q(body__icontains = q)|
-            Q(title__icontains = q))
+            Q(title__icontains = q)).values_list('id', flat = True)
+        #prepare queryset of helpers of the indexed paragraph type in the list
+        helpers = StreamHelper.objects.filter(stream_type='IndexedParagraph',
+            stream_id__in=paragraphs, )
         #blog content type
         bl_type = ContentType.objects.get(app_label='blog', model='article').id
         #filter paragraphs by blog type
-        bl_paragraphs = paragraphs.filter( parent_type = bl_type )
+        bl_helpers = helpers.filter( obj_type = bl_type )
         #extract list of blogs
-        bl_list = bl_paragraphs.values_list('parent_id', flat = True)
+        bl_list = bl_helpers.values_list('obj_id', flat = True)
         #prepare list of user uploads referenced by blog posts
         uploads = UserUpload.objects.filter(body__icontains = q)
         up_list = uploads.values_list('post_id', flat = True)
@@ -33,9 +37,9 @@ def search_results(request):
         #TreePage page content type
         tp_type = ContentType.objects.get(app_label='pages', model='treepage').id
         #filter paragraphs by blog type
-        tp_paragraphs = paragraphs.filter( parent_type = tp_type )
+        tp_helpers = helpers.filter( obj_type = tp_type )
         #extract list of TreePage
-        tp_list = tp_paragraphs.values_list('parent_id', flat = True)
+        tp_list = tp_helpers.values_list('obj_id', flat = True)
         pages = TreePage.objects.filter(Q(title__icontains=q)|
             Q(intro__icontains=q)|Q(id__in = tp_list))
         if pages:
