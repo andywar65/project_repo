25edd74c3d0d029e5050757aab950c1621b4f9b2
andywar65 <andy.war1965@gmail.com>
2020-04-06 22:57:21 +0200

    adding other social links to processor, footer and secrets

--- project/processors.py
+++ project/processors.py
@@ -4,6 +4,8 @@ def get_global_settings(request):
     website = {'name': settings.WEBSITE_NAME,
         'acro': settings.WEBSITE_ACRO,
         'facebook': settings.FB_LINK,
+        'insta': settings.INSTA_LINK,
+        'linked': settings.IN_LINK,
         'github': settings.GITHUB_LINK,
         'external': settings.EXT_LINK,
         }
