3c8aed8cf711bfacdb2bee9f4ae1a7107ba0ae2e
andywar65 <andy.war1965@gmail.com>
2020-05-09 12:04:20 +0200

    deleting old tests

--- pages/tests.py
+++ pages/tests.py
@@ -1,31 +1,3 @@
-from datetime import datetime
 from django.test import TestCase
-from .models import (Location, )
 
-class LocationTestCase(TestCase):
-    #@classmethod
-    #def setUpTestData(cls):
-    def setUp(self):
-        Location.objects.create(title="Marco",
-            #image = cls.img,
-            address="Via Agnelli",
-            gmap_link = 'https://goo.gl/maps/jLKe3iur2EtL8zuB7',
-            gmap_embed = '<scrap>https://goo.gl/maps/jLKe3iur2EtL8zuB7</scrap>',
-            body = 'Body')
-
-    def test_name(self):
-        """Name is correctly identified"""
-        luogo = Location.objects.get(title="Marco")
-        self.assertEqual(luogo.__str__(), 'Marco')
-
-    def test_link(self):
-        """Link is correctly identified"""
-        luogo = Location.objects.get(title="Marco")
-        self.assertEqual(luogo.get_gmap_link(),
-            '<a href="https://goo.gl/maps/jLKe3iur2EtL8zuB7" class="btn" target="_blank">Mappa</a>')
-
-    def test_thumb(self):
-        """Thumbnail is correctly identified"""
-        luogo = Location.objects.get(title="Marco")
-        self.assertEqual(luogo.get_thumb(),
-            '')
+# Create your tests here.
--- users/tests.py
+++ users/tests.py
@@ -1,11 +1,3 @@
 from django.test import TestCase
-from .models import (CourseSchedule, )
 
-class CourseScheduleTestCase(TestCase):
-    def setUp(self):
-        CourseSchedule.objects.create(full="Lunedì 16:00", abbrev="1-LU16")
-
-    def test_name(self):
-        """Name is correctly identified"""
-        orario = CourseSchedule.objects.get(abbrev="1-LU16")
-        self.assertEqual(orario.__str__(), 'Lunedì 16:00')
+# Create your tests here.
