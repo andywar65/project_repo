8e5e84241c346d16eaa323801abd1e8d8f21628a
andywar65 <andy.war1965@gmail.com>
2020-05-09 12:50:34 +0200

    added test article str method

--- blog/tests/test_models.py
+++ blog/tests/test_models.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+from blog.models import Article
+
+class ArticleModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        Article.objects.create(title='Article 1')
+
+    def test_article_str_method(self):
+        article = Article.objects.get(id = 1)
+        self.assertEquals(article.__str__(), 'Article 1')
