204efa5a0dd999bf3c2e90b8b9646f053465f010
andywar65 <andy.war1965@gmail.com>
2020-05-11 00:05:19 +0200

    added user and profile name methods

--- users/tests/test_models.py
+++ users/tests/test_models.py
@@ -1,52 +1,56 @@
 from django.test import TestCase
 
-from streamblocks.models import IndexedParagraph
-from pages.models import TreePage
+from users.models import User, Profile
 
-class TreePageModelTest(TestCase):
+class UserModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         # Set up non-modified objects used by all test methods
-        IndexedParagraph.objects.create(title='Foo', body='Bar')
-        TreePage.objects.create(title='Page 1', path = '0001', depth = 1,
-            numchild = 2,
-            #stream = '[{"unique_id":"4h5dps","model_name":"IndexedParagraph","id":1,"options":{}}]',
-            )
-        TreePage.objects.create(title='Child Page', path = '00010001',
-            depth = 2,
-            numchild = 0,
-            )
-        TreePage.objects.create(title='Second Child Page', path = '00010002',
-            depth = 2,
-            numchild = 0,
-            )
+        # user andywar65 with id = 1 already created in blog test suite
+        User.objects.create(username='andy.war65', password='P4s5W0r6',
+            first_name='Andrea', last_name='Guerra')
+        User.objects.create(username='rawydna56', password='P4s5W0r6',)
+        profile = Profile.objects.get(pk=2)
+        profile.avatar = 'uploads/users/avatar.jpg'
+        profile.save()
 
-    def test_tree_page_str_method(self):
-        page = TreePage.objects.get(id = 1)
-        self.assertEquals(page.__str__(), 'Page 1')
+    def test_user_get_full_name(self):
+        user = User.objects.get(id = 2)
+        self.assertEquals(user.get_full_name(), 'Andrea Guerra')
 
-    def test_tree_page_get_path(self):
-        page = TreePage.objects.get(id = 1)
-        #this test also generate unique slug
-        self.assertEquals(page.get_path(), '/docs/page-1/')
+    def test_user_get_full_username(self):
+        user = User.objects.get(id = 3)
+        self.assertEquals(user.get_full_name(), 'rawydna56')
 
-    def test_child_tree_page_get_path(self):
-        page = TreePage.objects.get(id = 2)
-        #this test also generate unique slug
-        self.assertEquals(page.get_path(), '/docs/page-1/child-page/')
+    def test_user_get_short_name(self):
+        user = User.objects.get(id = 2)
+        self.assertEquals(user.get_short_name(), 'Andrea')
 
-    def test_tree_page_get_adjacent_pages(self):
-        page = TreePage.objects.get(id = 1)
-        child = TreePage.objects.get(id = 2)
-        self.assertEquals(page.get_adjacent_pages(), (None, child))
+    def test_user_get_short_username(self):
+        user = User.objects.get(id = 3)
+        self.assertEquals(user.get_short_name(), 'rawydna56')
 
-    def test_child_tree_page_get_adjacent_pages(self):
-        parent = TreePage.objects.get(id = 1)
-        child = TreePage.objects.get(id = 2)
-        sibling = TreePage.objects.get(id = 3)
-        self.assertEquals(child.get_adjacent_pages(), (parent, sibling))
+    def test_profile_get_full_name(self):
+        profile = Profile.objects.get(pk = 2)
+        self.assertEquals(profile.get_full_name(), 'Andrea Guerra')
 
-    #def test_tree_page_stream_search(self):
-        #page = TreePage.objects.get(id = 1)
-        #self.assertEquals(page.stream_search,
-            #'\n  \n    Foo\n    \n  \n  \n     Bar \n  \n\n')
+    def test_profile_get_full_username(self):
+        profile = Profile.objects.get(pk = 3)
+        self.assertEquals(profile.get_full_name(), 'rawydna56')
+
+    def test_profile_str_full_name(self):
+        profile = Profile.objects.get(pk = 2)
+        self.assertEquals(profile.__str__(), 'Andrea Guerra')
+
+    def test_profile_str_username(self):
+        profile = Profile.objects.get(pk = 3)
+        self.assertEquals(profile.__str__(), 'rawydna56')
+
+    def test_profile_get_thumb(self):
+        profile = Profile.objects.get(pk = 2)
+        # here extracting path from FileObject for convenience
+        self.assertEquals(profile.get_thumb().path, 'uploads/users/avatar.jpg')
+
+    def test_profile_get_no_thumb(self):
+        profile = Profile.objects.get(pk = 3)
+        self.assertEquals(profile.get_thumb(), None)
