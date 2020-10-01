57b6c11590c37ee3aa3511671dbfebc0bd25cdd8
andywar65 <andy.war1965@gmail.com>
2020-10-01 18:08:19 +0200

    add user upload permission added to trusted group

--- blog/apps.py
+++ blog/apps.py
@@ -3,8 +3,10 @@ from django.db.models.signals import post_migrate
 
 def create_trusted_group(sender, **kwargs):
     from django.contrib.auth.models import Permission, Group
-    obj, created = Group.objects.get_or_create(name='Trusted')
-    pass
+    grp, created = Group.objects.get_or_create(name='Trusted')
+    if created:
+        permission = Permission.objects.filter(codename="add_userupload")
+        grp.permissions.set(permission)
 
 class BlogConfig(AppConfig):
     name = 'blog'
