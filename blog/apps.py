efca908a4a12b28e28095f3a456286052f7583c5
andywar65 <andy.war1965@gmail.com>
2020-10-01 17:41:31 +0200

    added post migrate signal to add trusted group

--- blog/apps.py
+++ blog/apps.py
@@ -1,5 +1,13 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
+def create_trusted_group(sender, **kwargs):
+    from django.contrib.auth.models import Permission, Group
+    obj, created = Group.objects.get_or_create(name='Trusted')
+    pass
 
 class BlogConfig(AppConfig):
     name = 'blog'
+
+    def ready(self):
+        post_migrate.connect(create_trusted_group, sender=self)
