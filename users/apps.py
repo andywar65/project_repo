d9d05ccd4d1885ddb0887f764a0a5dc56e9dc8d3
andywar65 <andy.war1965@gmail.com>
2020-10-14 13:47:55 +0200

    added user and profile manager groups on migration

--- users/apps.py
+++ users/apps.py
@@ -1,5 +1,23 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
+def create_app_related_groups(sender, **kwargs):
+    from django.contrib.auth.models import Permission, Group
+    grp, created = Group.objects.get_or_create(name='User Manager')
+    if created:
+        permissions = Permission.objects.filter(codename__in=('view_user',
+         'add_user', 'change_user', 'view_usermessage', 'change_usermessage',
+         'delete_usermessage'))
+        grp.permissions.set(permissions)
+    grp, created = Group.objects.get_or_create(name='Profile Manager')
+    if created:
+        permissions = Permission.objects.filter(codename__in=('view_profile',
+            'change_profile', 'view_usermessage', 'change_usermessage',
+            'delete_usermessage'))
+        grp.permissions.set(permissions)
 
 class UsersConfig(AppConfig):
     name = 'users'
+
+    def ready(self):
+        post_migrate.connect(create_app_related_groups, sender=self)
