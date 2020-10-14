bb2b30a47a4ff95daa9ba961ea7887fbc9a65cca
andywar65 <andy.war1965@gmail.com>
2020-10-14 13:09:34 +0200

    using one function to create app related groups

--- blog/apps.py
+++ blog/apps.py
@@ -1,15 +1,12 @@
 from django.apps import AppConfig
 from django.db.models.signals import post_migrate
 
-def create_trusted_group(sender, **kwargs):
+def create_app_related_groups(sender, **kwargs):
     from django.contrib.auth.models import Permission, Group
     grp, created = Group.objects.get_or_create(name='Trusted')
     if created:
         permission = Permission.objects.filter(codename="add_userupload")
         grp.permissions.set(permission)
-
-def create_editor_group(sender, **kwargs):
-    from django.contrib.auth.models import Permission, Group
     grp, created = Group.objects.get_or_create(name='Editors')
     if created:
         permissions = Permission.objects.filter(codename__in=('view_article',
@@ -21,5 +18,4 @@ class BlogConfig(AppConfig):
     name = 'blog'
 
     def ready(self):
-        post_migrate.connect(create_trusted_group, sender=self)
-        post_migrate.connect(create_editor_group, sender=self)
+        post_migrate.connect(create_app_related_groups, sender=self)
