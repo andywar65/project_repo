642cd42321919ef6e799e30aa5b36f4b9ee8653e
andywar65 <andy.war1965@gmail.com>
2020-10-14 13:06:27 +0200

    added editors group on migration

--- blog/apps.py
+++ blog/apps.py
@@ -8,8 +8,18 @@ def create_trusted_group(sender, **kwargs):
         permission = Permission.objects.filter(codename="add_userupload")
         grp.permissions.set(permission)
 
+def create_editor_group(sender, **kwargs):
+    from django.contrib.auth.models import Permission, Group
+    grp, created = Group.objects.get_or_create(name='Editors')
+    if created:
+        permissions = Permission.objects.filter(codename__in=('view_article',
+         'add_article', 'change_article', 'delete_article', 'view_userupload',
+         'add_userupload', 'change_userupload', 'delete_userupload'))
+        grp.permissions.set(permissions)
+
 class BlogConfig(AppConfig):
     name = 'blog'
 
     def ready(self):
         post_migrate.connect(create_trusted_group, sender=self)
+        post_migrate.connect(create_editor_group, sender=self)
