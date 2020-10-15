c8f030f4e58bc57df9f910259f9444b9f4c9949b
andywar65 <andy.war1965@gmail.com>
2020-10-15 21:21:27 +0200

    adding portfolio directory to gitignore

--- project/settings/base.py
+++ project/settings/base.py
@@ -59,7 +59,6 @@ INSTALLED_APPS = [
     'crispy_forms',
     'treebeard',
     'private_storage',
-    'django_crontab',
 ]
 
 MIDDLEWARE = [
@@ -92,8 +91,6 @@ TEMPLATES = [
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-
-CRONJOBS = [ ('*/10 * * * *', 'accounting.cron.fetch_csvinvoice_attachments'),]
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
