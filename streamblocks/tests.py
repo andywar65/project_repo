8668fb3eecf0d6a158686654925b383dbaee1e6f
andywar65 <andy.war1965@gmail.com>
2020-10-25 14:47:36 +0100

    commenting out streamblock tests

--- streamblocks/tests.py
+++ streamblocks/tests.py
@@ -1,19 +1,19 @@
 from django.test import TestCase
 
-from .models import DownloadableFile
+#from .models import DownloadableFile
 
-class StreamblocksModelTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        DownloadableFile.objects.create(id=13,
-            fb_file='documents/document.pdf', description='Foo Bar')
-        DownloadableFile.objects.create(id=14,
-            fb_file='documents/document.pdf')
+#class StreamblocksModelTest(TestCase):
+    #@classmethod
+    #def setUpTestData(cls):
+        #DownloadableFile.objects.create(id=13,
+            #fb_file='documents/document.pdf', description='Foo Bar')
+        #DownloadableFile.objects.create(id=14,
+            #fb_file='documents/document.pdf')
 
-    def test_download_file_get_description(self):
-        download = DownloadableFile.objects.get(id=13)
-        self.assertEquals(download.get_description(), 'Foo Bar')
+    #def test_download_file_get_description(self):
+        #download = DownloadableFile.objects.get(id=13)
+        #self.assertEquals(download.get_description(), 'Foo Bar')
 
-    def test_download_file_get_no_description(self):
-        download = DownloadableFile.objects.get(id=14)
-        self.assertEquals(download.get_description(), 'document.pdf')
+    #def test_download_file_get_no_description(self):
+        #download = DownloadableFile.objects.get(id=14)
+        #self.assertEquals(download.get_description(), 'document.pdf')
