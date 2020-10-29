17beeae299eb7ea4fcb48dac4b8154adbb6d5941
andywar65 <andy.war1965@gmail.com>
2020-10-29 21:55:29 +0100

    adding rest framework to installed apps

--- project/settings/base.py
+++ project/settings/base.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'treebeard',
     'private_storage',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -111,6 +112,12 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+    'rest_framework.permissions.IsAdminUse',
+    ),
+}
 
 STATICFILES_DIRS = [
     PROJECT_DIR / "static",
