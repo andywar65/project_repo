b5dd8073a3455085887776fc0f9820f93063d1a8
andywar65 <andy.war1965@gmail.com>
2020-11-04 14:54:02 +0100

    i18n of blog forms

--- blog/forms.py
+++ blog/forms.py
@@ -1,17 +1,19 @@
 from django import forms
 from django.forms import ModelForm
+from django.utils.translation import gettext as _
+
 from .models import UserUpload, Article
 from users.models import User
 
 class UserUploadForm(ModelForm):
-    body = forms.CharField(label = 'Testo',
-        widget=forms.Textarea(attrs={'placeholder': "Scrivi qui il messaggio"}))
+    body = forms.CharField(label = _('Text'),
+        widget=forms.Textarea(attrs={'placeholder': _("Leave here your message")}))
     class Meta:
         model = UserUpload
         fields = ('image', 'body',)
 
 class ArticleForm(ModelForm):
-    author = forms.ModelChoiceField(label="Autore", required = False,
+    author = forms.ModelChoiceField(label=_("Author"), required = False,
         queryset = User.objects.filter(is_active = True, ).order_by('username'), )
 
     class Meta:
