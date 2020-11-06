029dbcb3bc042feecfaf84973aa59c5f4b0530b0
andywar65 <andy.war1965@gmail.com>
2020-11-06 20:24:53 +0100

    i18n pages models

--- pages/models.py
+++ pages/models.py
@@ -4,35 +4,31 @@ import json
 from django.utils.timezone import now
 from django.utils.html import strip_tags
 from django.db import models
+from django.utils.translation import gettext as _
 
 from filebrowser.fields import FileBrowseField
 from treebeard.mp_tree import MP_Node
 
 from project.utils import generate_unique_slug
-#from portfolio.models import Project
 from blog.models import Article
-#from streamfield.base import StreamObject
-#from streamfield.fields import StreamField
-#from streamblocks.models import (IndexedParagraph, CaptionedImage, Gallery,
-    #LandscapeGallery, DownloadableFile, LinkableList, BoxedText, HomeButton)
 
 class HomePage(models.Model):
 
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField('Titolo',
-        help_text="Compare sulla prima immagine",
+    title = models.CharField(_('Title'),
+        help_text=_("Appears on first image"),
         max_length = 50, null=True, blank=True)
-    intro = models.CharField('Sottotitolo', max_length = 100,
-        null=True, blank=True, help_text = 'Il sito in due parole')
-    body = models.TextField('Testo',
-        null=True, blank=True, help_text = 'Un testo di presentazione')
-    date = models.DateTimeField('Data:', default = now, )
+    intro = models.CharField(_('Subtitle'), max_length = 100,
+        null=True, blank=True, help_text = _('Website in few words'))
+    body = models.TextField(_('Text'),
+        null=True, blank=True, help_text = _('Talk about this website'))
+    date = models.DateTimeField(_('Date:'), default = now, )
 
     def __str__(self):
         return self.title if self.title else str(self.uuid)
 
     class Meta:
-        verbose_name = 'Home Page'
+        verbose_name = _('Home Page')
         ordering = ('-date', )
 
 class GalleryImage(models.Model):
@@ -42,46 +38,42 @@ class GalleryImage(models.Model):
         #on_delete = models.CASCADE, related_name='project_image')
     post = models.ForeignKey(Article, null=True, editable=False,
         on_delete = models.CASCADE, related_name='article_image')
-    image = models.ImageField("Immagine", max_length=200, editable = False,
+    image = models.ImageField(_("Images"), max_length=200, editable = False,
         null=True, upload_to='uploads/images/galleries/')
-    fb_image = FileBrowseField("Immagine", max_length=200,
+    fb_image = FileBrowseField(_("Images"), max_length=200,
         extensions=[".jpg", ".png", ".jpeg", ".gif", ".tif", ".tiff"],
         null=True, directory='images/galleries/')
-    caption = models.CharField("Didascalia", max_length = 200, blank=True,
+    caption = models.CharField(_("Caption"), max_length = 200, blank=True,
         null=True)
-    position = models.PositiveSmallIntegerField("Posizione", null=True)
+    position = models.PositiveSmallIntegerField(_("Position"), null=True)
 
     class Meta:
-        verbose_name="Immagine"
-        verbose_name_plural="Immagini"
+        verbose_name=_("Image")
+        verbose_name_plural=_("Images")
 
 class HomeButton(models.Model):
     home = models.ForeignKey(HomePage, null=True, editable=False,
         on_delete = models.CASCADE, related_name='home_button')
-    title = models.CharField("Titolo", max_length = 100, null=True )
-    subtitle = models.CharField("Sottotitolo", max_length = 200, null=True )
-    link = models.URLField("Link", max_length = 200, null=True, )
-    position = models.PositiveSmallIntegerField("Posizione", null=True)
+    title = models.CharField(_('Title'), max_length = 100, null=True )
+    subtitle = models.CharField(_('Subtitle'), max_length = 200, null=True )
+    link = models.URLField(_('Link'), max_length = 200, null=True, )
+    position = models.PositiveSmallIntegerField(_("Position"), null=True)
 
     class Meta:
-        verbose_name="Pulsante di Home Page"
-        verbose_name_plural="Pulsanti di Home Page"
+        verbose_name=_("Home Page button")
+        verbose_name_plural=_("Home Page buttons")
 
 class TreePage(MP_Node):
-    title = models.CharField('Titolo', max_length = 50)
-    slug = models.SlugField('Slug', max_length=50, null=True, blank=True,
+    title = models.CharField(_('Title'), max_length = 50)
+    slug = models.SlugField(_('Slug'), max_length=50, null=True, blank=True,
         unique = True,
-        help_text = """Titolo come appare nell'indirizzo della pagina,
-            solo lettere minuscole e senza spazi""")
-    intro = models.CharField('Introduzione',
+        help_text = _("""Title as it appears in the address bar,
+            only lowercas, no blank spaces"""))
+    intro = models.CharField(_('Introduction'),
         blank= True, null=True, max_length = 200)
-    body = models.TextField('Testo', null=True, blank=True,)
-    #stream = StreamField( model_list=[ IndexedParagraph, CaptionedImage,
-        #Gallery, DownloadableFile, LinkableList, BoxedText, ],
-        #verbose_name="Testo" )
-    #stream_search = models.TextField(editable=False, null=True)
-    summary = models.BooleanField('Mostra sommario', default = True, )
-    navigation = models.BooleanField('Mostra navigazione', default = True, )
+    body = models.TextField(_('Text'), null=True, blank=True,)
+    summary = models.BooleanField(_('Show summary'), default = True, )
+    navigation = models.BooleanField(_('Show navigation'), default = True, )
     last_updated = models.DateTimeField(editable=False, null=True)
     paragraphs = models.JSONField(editable=False, null=True, )
 
@@ -145,5 +137,5 @@ class TreePage(MP_Node):
         return self.title
 
     class Meta:
-        verbose_name = 'Pagina ad albero'
-        verbose_name_plural = 'Pagine ad albero'
+        verbose_name = _('Tree page')
+        verbose_name_plural = _('Tree pages')
