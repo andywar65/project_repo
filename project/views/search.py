2d1bf165c226865352c76d63b40227250e4258d6
andywar65 <andy.war1965@gmail.com>
2020-11-07 16:51:31 +0100

    i18n project urls, views, templates

--- project/views/search.py
+++ project/views/search.py
@@ -5,7 +5,6 @@ from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
 
 from blog.models import Article, UserUpload
 from pages.models import TreePage
-#from portfolio.models import Project
 
 class ValidateForm(forms.Form):
     q = forms.CharField(max_length=100)
@@ -31,17 +30,12 @@ def search_results(request):
             articles = articles.order_by('-rank')
             success = True
         #search in pages
-        #v = SearchVector('title', 'intro', 'stream_rendered')
         pages = TreePage.objects.annotate(rank=SearchRank(v, q))
         pages = pages.filter(rank__gt=0.01)
         if pages:
             pages = pages.order_by('-rank')
             success = True
-        #progs = Project.objects.annotate(rank=SearchRank(v, q))
-        #progs = progs.filter(rank__gt=0.01)
-        #if progs:
-            #progs = progs.order_by('-rank')
-            #success = True
+
         return render(request, 'search_results.html',
             {'search': request.GET['q'], 'all_uploads': article_uploads,
             'all_blogs': articles, 'pages': pages, #'progs': progs,
