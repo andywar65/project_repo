aa33596a260f7320d6c5d8d280ed4b7310cbb613
andywar65 <andy.war1965@gmail.com>
2020-11-09 18:24:22 +0100

    i18n users apps

--- users/apps.py
+++ users/apps.py
@@ -1,15 +1,16 @@
 from django.apps import AppConfig
 from django.db.models.signals import post_migrate
+from django.utils.translation import gettext as _
 
 def create_app_related_groups(sender, **kwargs):
     from django.contrib.auth.models import Permission, Group
-    grp, created = Group.objects.get_or_create(name='User Manager')
+    grp, created = Group.objects.get_or_create(name=_('User Manager'))
     if created:
         permissions = Permission.objects.filter(codename__in=('view_user',
          'add_user', 'change_user', 'view_usermessage', 'change_usermessage',
          'delete_usermessage'))
         grp.permissions.set(permissions)
-    grp, created = Group.objects.get_or_create(name='Profile Manager')
+    grp, created = Group.objects.get_or_create(name=_('Profile Manager'))
     if created:
         permissions = Permission.objects.filter(codename__in=('view_profile',
             'change_profile', 'view_usermessage', 'change_usermessage',
