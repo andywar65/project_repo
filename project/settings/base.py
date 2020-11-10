13bdc3337a140ee89d61c8aeb2c936e575feda83
andywar65 <andy.war1965@gmail.com>
2020-11-10 09:45:57 +0100

    deleted gettext in base setting

--- project/settings/base.py
+++ project/settings/base.py
@@ -14,7 +14,6 @@ from pathlib import Path
 import json
 
 from django.core.exceptions import ImproperlyConfigured
-from django.utils.translation import gettext as _
 
 PROJECT_DIR = Path(__file__).resolve(strict=True).parent.parent
 BASE_DIR = Path(PROJECT_DIR).resolve(strict=True).parent
@@ -135,10 +134,10 @@ FILEBROWSER_VERSIONS = {
     'medium': {'verbose_name': 'Medium (4col )', 'width': 300, 'height': '', 'opts': ''},
     'big': {'verbose_name': 'Big (6 col)', 'width': 460, 'height': '', 'opts': ''},
     'large': {'verbose_name': 'Large (8 col)', 'width': 680, 'height': '', 'opts': ''},
-    'wide_landscape': {'verbose_name': _('Landscape 2:1'), 'width': 1600, 'height': 800, 'opts': 'crop'},
-    'landscape': {'verbose_name': _('Landscape 4:3'), 'width': 1280, 'height': 720, 'opts': 'crop'},
-    'portrait': {'verbose_name': _('Portrait'), 'width': 768, 'height': 1024, 'opts': 'crop'},
-    'square': {'verbose_name': _('Square'), 'width': 768, 'height': 768, 'opts': 'crop'},
+    'wide_landscape': {'verbose_name': 'Landscape 2:1', 'width': 1600, 'height': 800, 'opts': 'crop'},
+    'landscape': {'verbose_name': 'Landscape 4:3', 'width': 1280, 'height': 720, 'opts': 'crop'},
+    'portrait': {'verbose_name': 'Portrait', 'width': 768, 'height': 1024, 'opts': 'crop'},
+    'square': {'verbose_name': 'Square', 'width': 768, 'height': 768, 'opts': 'crop'},
     }
 
 FETCH_EMAILS = True
