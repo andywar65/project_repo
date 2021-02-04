f0e7e5568d56951051d913d00841682adc8da5d0
andywar65 <andy.war1965@gmail.com>
2021-02-04 23:11:15 +0100

    fixing navbar

--- project/settings/base.py
+++ project/settings/base.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
     'accounting.apps.AccountingConfig',
     'portfolio.apps.PortfolioConfig',
     'buildings.apps.BuildingsConfig',
+    'bimblog.apps.BimblogConfig',
 ]
 
 MIDDLEWARE = [
