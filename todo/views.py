26ec9cfe44cdea4251fae2d4df3bfa1a9ef8e4ea
andywar65 <andy.war1965@gmail.com>
2021-11-03 23:28:40 +0100

    adding perms to template

--- todo/views.py
+++ todo/views.py
@@ -8,6 +8,15 @@ from .serializers import TaskSerializer
 class TaskTemplateView(TemplateView):
     template_name = 'todo/home.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['perms'] = {
+            'add': self.request.user.has_perm('todo.add_task'),
+            'change': self.request.user.has_perm('todo.change_task'),
+            'delete': self.request.user.has_perm('todo.delete_task'),
+        }
+        return context
+
 
 class TaskUpdateView(generics.RetrieveUpdateAPIView):
     queryset = Task.objects.all()
